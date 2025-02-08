c84bff206195140340ed777b4173b1aaa1337a3b
Tolga <tolgakoluacik@outlook.com>
2025-02-08 10:14:37 +0300

    added mock login operations

---
+++ lib/models/user.dart
@@ -0,0 +1,20 @@
+class UserModel {
+  final String id;
+  final String email;
+
+  UserModel({required this.id, required this.email});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'] as String,
+      email: json['email'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+    };
+  }
+}
--- lib/widgets/login_button.dart
+++ lib/widgets/login_button.dart
@@ -1,8 +1,10 @@
 import 'package:asset_tracker/constants/theme_colors.dart';
+import 'package:asset_tracker/views/home.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import '../constants/app_edge_insets.dart';
-import '../services/firebase_auth.dart';
+import '../services/auth_service.dart';
+import '../services/mock_auth_service.dart';
 
 class LoginButton extends StatelessWidget {
   final GlobalKey<FormState> formKey;
@@ -24,11 +26,29 @@ class LoginButton extends StatelessWidget {
         final email = emailController.text;
         final password = passwordController.text;
 
-        await signInWithEmailAndPassword(
-          email,
-          password,
-          context
-        );
+        try {
+          final user = await AuthService.authService.signInWithEmailAndPassword(
+            email,
+            password,
+          );
+
+          if (user != null) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => HomeScreen(email: user.email)),
+            );
+          }
+        } catch (e) {
+          if (e is AuthException) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(e.code.tr())),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('default-error'.tr())),
+            );
+          }
+        }
       }
     },
       icon: const Icon(Icons.login, color: AppColors.mainButtonTextColor,),
